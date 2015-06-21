7c355f9aed8e6b87b82b4877d106ca7c34ca6a52
ashumeow <aswini1405@outlook.com>
2015-06-21 17:30:27 +0530

    push plot5

---
+++ Week-3/course-project-2/plot5.R
@@ -0,0 +1,19 @@
+# Loading ggplot2
+library(ggplot2)
+
+# Loading data
+NEI <- readRDS("exp-data/summarySCC_PM25.rds")
+NEI$year <- factor(NEI$year, levels = c('1999', '2002', '2005', '2008'))
+SCC <- readRDS("exp-data/Source_Classification_Code.rds")
+
+# Baltimore City, Maryland == fips
+MD.onroad <- subset(NEI, fips == 24510 & type == 'ON-ROAD')
+
+# Aggregation
+MD.df <- aggregate(MD.onroad[, 'Emissions'], by = list(MD.onroad$year), sum)
+colnames(MD.df) <- c('year', 'Emissions')
+
+# Final results :)
+png('exp-data/xxx-2/plot5.png')
+ggplot(data = MD.df, aes(x = year, y = Emissions)) + geom_bar(aes(fill = year), stat = "identity") + guides(fill = F) + ggtitle('Total Emissions of Motor Vehicle Sources in Baltimore City, Maryland') + ylab(expression('PM'[2.5])) + xlab('Year') + theme(legend.position = 'none') + geom_text(aes(label = round(Emissions, 0), size = 1, hjust = 0.5, vjust = 2))
+dev.off()
